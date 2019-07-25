6f0f228a49732c5648072a8139df87bedc153578
mchiszar <52429587+mchiszar@users.noreply.github.com>
2019-07-25 14:47:34 -0500

    lesson-02

--- app/src/main/java/com/example/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.example.coderswag.Model
 import android.support.v7.widget.AppCompatImageButton
 import android.support.v7.widget.DialogTitle
 
-class Category (val title: String, val image: String)}
+class Category (val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
