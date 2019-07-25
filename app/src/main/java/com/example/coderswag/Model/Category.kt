58f36e250b57e0f85db8099704968ff5e4261dbc
mchiszar <52429587+mchiszar@users.noreply.github.com>
2019-07-25 12:11:28 -0500

    lesson 01

---
+++ app/src/main/java/com/example/coderswag/Model/Category.kt
@@ -0,0 +1,6 @@
+package com.example.coderswag.Model
+
+import android.support.v7.widget.AppCompatImageButton
+import android.support.v7.widget.DialogTitle
+
+class Category (val title: String, val image: String)}
